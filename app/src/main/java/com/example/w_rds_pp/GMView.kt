f03b4359a3c154b8728cb92dd7feeafa41e0d5d6
anonymous <anonymous>
2022-05-24 22:03:04 +0200

    on letter selected, disable underline

--- app/src/main/java/com/example/w_rds_pp/GMView.kt
+++ app/src/main/java/com/example/w_rds_pp/GMView.kt
@@ -7,16 +7,21 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import java.util.LinkedList
 
 // todo gm len(minor) should == len(major)
+
+data class BC(val ds: Pair<Char, Char>, val x: Float, val y: Float, val xMax: Float, val yMax: Float)
 
 class GMView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     var gm: GM = GM("_p_ _p_ _ q_ e_r__", "aqb cqa h ph rhegt")
 
-    var onLetterSelected: (Char, Int) -> Int = { c, i ->
-        Log.d(TAG, "onLetterSelected: c=${c}, i=${i}")
+    var onLetterSelected: (Pair<Char, Char>) -> Int = { ds ->
+        Log.d(TAG, "onLetterSelected: ds=${ds}")
     }
+
     private val minorPaint = Paint()
     private val majorPaint = Paint()
     private val underlinePaint = Paint()
@@ -31,6 +36,7 @@ class GMView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private val charSpace = 5
 
+    private var boxesCords: List<BC> = listOf()
 
     init {
         isFocusable = true
@@ -105,12 +111,14 @@ class GMView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
         val deltaX = majorLetterSize + charSpace
 
-        fun printUnderline(){
-            val path = Path()
-            path.moveTo(x, y + majorLetterSize + majorMinorYSpace)
-            path.lineTo(x + majorLetterSize, y + majorLetterSize + majorMinorYSpace)
-            canvas.drawPath(path, underlinePaint)
-        }
+        val boxesCords = mutableListOf<BC>()
+
+//        fun printUnderline(){
+//            val path = Path()
+//            path.moveTo(x, y + majorLetterSize + majorMinorYSpace)
+//            path.lineTo(x + majorLetterSize, y + majorLetterSize + majorMinorYSpace)
+//            canvas.drawPath(path, underlinePaint)
+//        }
 
         fun printMajorCharacter(c: Char){
             canvas.drawText(charArrayOf(c), 0, 1, x, y, majorPaint)
@@ -129,12 +137,17 @@ class GMView(context: Context, attrs: AttributeSet) : View(context, attrs) {
             x += deltaX
         }
 
+        fun addToBoxesCord(ds: Pair<Char, Char>) {
+            boxesCords.add(BC(ds, x, y - majorLetterSize, x + majorLetterSize, y + deltaY - lineSpacing))
+        }
+
         for(line in majorProcessedText) {
             for(word in line) {
                 for(ds in word) {
                     printMajorCharacter(ds.first)
                     printMinorCharacter(ds.second)
-                    printUnderline()
+//                    printUnderline()
+                    addToBoxesCord(ds)
                     moveToNextCharacter()
                 }
                 moveToNextCharacter() // space
@@ -142,13 +155,31 @@ class GMView(context: Context, attrs: AttributeSet) : View(context, attrs) {
             moveToNewLine()
         }
 
-
+        this.boxesCords = boxesCords
 
     }
 
     override fun draw(canvas: Canvas?) {
         super.draw(canvas)
         drawText(canvas!!)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if(event.action == MotionEvent.ACTION_DOWN) {
+            val x = event.x
+            val y = event.y
+            Log.d(TAG, "onTouchEvent: ACTION DOWN, x: ${x}, y: ${y}")
+            val boxes = boxesCords.filter { x >= it.x && x <= it.xMax && y >= it.y && y <= it.yMax }
+            if(boxes.size == 0) {
+                Log.d(TAG, "onTouchEvent: 0 matching")
+                return true
+            } else if(boxes.size > 1) {
+                Log.d(TAG, "onTouchEvent: >1 matching, chosing first box, all boxes: " + boxes.fold("") { acc, bc -> "$acc$bc; " })
+            }
+            onLetterSelected(boxes[0].ds)
+        }
+
+        return true
     }
 
     companion object {
