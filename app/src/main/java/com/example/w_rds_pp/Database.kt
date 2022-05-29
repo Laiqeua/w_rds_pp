30f28be74bfa4d0a0c83ad0706051ab1e99dc5c2
anonymous <anonymous>
2022-05-29 21:40:40 +0200

    new quotes

--- app/src/main/java/com/example/w_rds_pp/Database.kt
+++ app/src/main/java/com/example/w_rds_pp/Database.kt
@@ -39,8 +39,8 @@ abstract class AppsDatabase : RoomDatabase() {
             if(OBJ != null) return OBJ!!
             synchronized(this) {
                 if(OBJ != null) return OBJ!!
-                OBJ = Room.databaseBuilder(context, AppsDatabase::class.java, "AppsDatabase1")
-                          .createFromAsset("populated_db.db")
+                OBJ = Room.databaseBuilder(context, AppsDatabase::class.java, "AppsDatabase2")
+                          .createFromAsset("quotes_db_1.db")
                           .build()
                 return OBJ!!
             }
