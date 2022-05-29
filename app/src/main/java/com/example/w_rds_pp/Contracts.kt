a0e0a02f4a56132166e29ec7eaa65d7b6af86e6d
anonymous <anonymous>
2022-05-29 21:51:20 +0200

    normalize db name, contracts value

--- app/src/main/java/com/example/w_rds_pp/Contracts.kt
+++ app/src/main/java/com/example/w_rds_pp/Contracts.kt
@@ -1,4 +1,4 @@
 package com.example.w_rds_pp
 
-const val GAME_SATE_PREF_NAME = "GameState3"
-const val CURRENT_GAME_STATE_PREF_KEY = "current-game"
+const val GAME_SATE_PREF_NAME = "GameStatePreferences"
+const val CURRENT_GAME_STATE_PREF_KEY = "CurrentGame"
